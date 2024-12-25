ab1df7ceab89657a807f2f81fc580980d67afaac
Daniil borisov <daiilborisov2001@gmail.com>
2024-12-25 13:11:53 +0300

    fix invalid excludes

--- exporter/fsstat.go
+++ exporter/fsstat.go
@@ -1,9 +1,11 @@
 package exporter
 
 import (
+	"fmt"
 	"io/fs"
 	"log"
 	"path/filepath"
+	"slices"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -22,19 +24,29 @@ func GetFSMetrics(cfg config.ExporterConfig) {
 	for _, v := range cfg.FilePaths {
 		go func() {
 			for {
+				start := time.Now()
 				var fileCount int
 				err := filepath.WalkDir(v, func(path string, d fs.DirEntry, err error) error {
-					switch d.IsDir() {
-					case true:
+					switch d.Type() {
+					case fs.ModeSymlink:
+						return nil
+					default:
+						switch d.IsDir() {
+						case true:
+							if slices.Contains(cfg.ExcludeFilePaths, path) {
+								return filepath.SkipDir
+							}
+						case false:
+							fileCount++
+						}
 
-					case false:
-						fileCount++
 					}
 					return nil
 				})
 				if err != nil {
 					log.Println(err)
 				}
+				fmt.Printf("%s\n", time.Since(start))
 				PathFileCount.WithLabelValues(v).Set(0)
 				PathFileCount.WithLabelValues(v).Set(float64(fileCount))
 				fileCount = 0
