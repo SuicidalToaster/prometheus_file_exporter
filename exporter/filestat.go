15de10bee7bdb690923dfafaca93d814ca35e2c9
Daniil Borisov <da.borisov@faberlic.com>
2025-01-14 11:19:34 +0300

    add hash

---
+++ exporter/filestat.go
@@ -0,0 +1,47 @@
+package exporter
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"io"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var FileHash = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "file_hash",
+}, []string{"hash", "path"})
+
+func GetFileList(p []string) {
+	for _, v := range p {
+		go func() {
+			for {
+				GetFileHash(v)
+				time.Sleep(5 * time.Second)
+			}
+		}()
+	}
+}
+
+func GetFileHash(p string) {
+	f, err := os.Open(p)
+	if err != nil {
+		FileHash.WithLabelValues("N/A", p).Set(0)
+	}
+	a, err := f.Stat()
+	defer f.Close()
+	if err != nil || a.IsDir() {
+		FileHash.WithLabelValues("N/A", p).Set(0)
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		FileHash.WithLabelValues("N/A", p).Set(0)
+	}
+	h := sha256.New()
+	h.Write(b)
+	s := base64.URLEncoding.EncodeToString(h.Sum(nil))
+	FileHash.WithLabelValues(s, p).Set(1)
+}
--- main.go
+++ main.go
@@ -3,19 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/SuicidalToaster/prometheus_file_exporter/config"
 	"github.com/SuicidalToaster/prometheus_file_exporter/exporter"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var conf = config.GetConfig()
-
 func main() {
-	start := time.Now()
+	conf := config.GetConfig()
 	go exporter.GetFSMetrics(conf)
-	fmt.Printf("%s", time.Since(start))
+	go exporter.GetFileList(conf.HashFiles)
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
